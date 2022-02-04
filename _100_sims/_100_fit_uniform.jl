75568e9143361f2faabca0286b2d1c34e03b68ba
Nurzhan Sapargali <nurzhan.sapargali@stat.uni-muenchen.de>
2022-02-04 16:28:41 +0600

    Recalculated derivatives

--- _100_sims/_100_fit_uniform.jl
+++ _100_sims/_100_fit_uniform.jl
@@ -1,41 +1,57 @@
+using Optim, NLSolversBase
+
+import StatsBase
+import Random
+
 import Plots
 import Distributions
-import StatsBase
 
 N = 1000;
 T = 5;
-n = 200;
+n = 50;
 
 
-function loglikelihood(K, T, alpha, beta)
-    betaT = beta + T;
+function loglikelihood(freqs, reps, params)
+    betaT = params[2] + reps;
     firstprod = 1;
     secondprod = 1;
-    for j in 1:T
-        firstprod *= (1.0 - (j - alpha) / betaT);
+    for j in 1:reps
+        firstprod *= (1.0 - (j - params[1]) / betaT);
         secondprod *= (1.0 - j / betaT);
     end
-    firstsum = sum([k * log(alpha + k) for k in values(K)]);
-    secondsum = log(betaT) * sum(values(K));
-    thirdsum = sum([log(1.0 - j / (alpha + k)) for k in values(K) for j in 1:k]);
-    fourthsum = sum([log(1.0 - (k - j) / betaT) for k in values(K) for j in 1:(T-k)]);
-    loglh = (length(K) * log(firstprod - secondprod) + firstsum + secondsum
+    firstsum = log(betaT) * sum(freqs);
+    secondsum = 0;
+    thirdsum = 0;
+    fourthsum = 0;
+    for k in freqs
+        secondsum += k * log(params[1] + k);
+        for j in 1:k
+            thirdsum += log(1.0 - j / (params[1] + k));
+        end
+        for j in 1:(reps - k)
+            fourthsum += log(1.0 - (k - j) / betaT);
+        end
+    end
+    loglh = (-length(freqs) * log(firstprod - secondprod) - firstsum + secondsum
              + thirdsum + fourthsum);
-    println(firstprod, ", ", secondprod, ", ",
-            firstsum, ", ", secondsum, ", ",
-            thirdsum, ", ", fourthsum, ", ",
-            loglh);
-    return loglh;
+    println("alpha = $(params[1]), beta = $(params[2]), loglh = $loglh")
+    return -loglh;
 end
 
-function main()
-    probs = collect(1:N) ./ sum(1:N);
-    x = StatsBase.sample(1:N, StatsBase.Weights(probs), n);
+function main(verbose = true)
+    Random.seed!(111);
+    x = StatsBase.sample(1:N, n, replace=false);
     for t in 2:T
-        append!(x, StatsBase.sample(1:N, StatsBase.Weights(probs), n));
+        append!(x, StatsBase.sample(1:N, n, replace=false));
     end
-    k = StatsBase.countmap(x);
-    println(loglikelihood(k, T, 1.0, 1.0));
+    K = values(StatsBase.countmap(x));
+    println("Optimisation started...");
+    f(x) = loglikelihood(K, T, x);
+    lower = [1e-8, 1e-8];
+    upper = [Inf, Inf];
+    opt = optimize(f, lower, upper, x0, NelderMead());
+    alpha, beta = Optim.minimizer(opt);
+    return
 end
 
 
