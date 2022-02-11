4b1379ab83e66aab865d7e0fb03e19f096a72baf
Nurzhan Sapargali <nurzhan@nurzh-pc.nurzh-pc>
2022-02-11 17:26:36 +0100

    Re-derive likelihood with constraint

--- _110_plot_likelihood.jl
+++ _110_plot_likelihood.jl
@@ -16,7 +16,7 @@ const PARAMS = [1.0 4.0; 1.0 1.0; 4.0 1.0];
 
 seed!(555);
 Plots.pyplot();
-alpha = beta = 0.1:0.5:15; # Alpha and beta values for plotting
+alpha = beta = 0.1:0.5:15.1; # Alpha and beta values for plotting
 for i in 1:size(PARAMS)[1]
     d = Beta(PARAMS[i, 1], PARAMS[i, 2]);
     p = rand(d, N);
@@ -25,21 +25,23 @@ for i in 1:size(PARAMS)[1]
     L(x, y) = likelihood_simple(x, y, K, T, "full");
     flabels = Dict(LL => "Log-likelihood", L => "Likelihood")
     for f in (L, LL)
-        title = "$(flabels[f]), true  α = $(PARAMS[i, 1]), β = $(PARAMS[i, 2])";
+        title = "α = $(PARAMS[i, 1]), β = $(PARAMS[i, 2])";
         Plots.plot(alpha, beta, f,
-                st=:contourf, title=title, fontsize=12);
-        Plots.plot!(size=(900, 900));
-        Plots.xlabel!("α");
-        Plots.ylabel!("β");
+                   st=:contourf, title=title, titlefontsize=24,
+                   xlabel="α", xtickfontsize=22, xguidefontsize=24,
+                   ylabel="β", ytickfontsize=22, yguidefontsize=24,
+                   legendfontsize=18, colorbar_tickfontsize=22,size=(1000, 900));
         Plots.scatter!([PARAMS[i, 1]], [PARAMS[i, 2]],
-                    color="lime", label="True α,β");
+                        color="lime", label="True α,β",
+                        markersize=12);
         Plots.savefig("$(FIGURES_FOLDER)$(flabels[f])_contplt_$i.pdf")
         Plots.plot(alpha, beta, f,
-                st=:surface, title=title, fontsize=12,
-                camera=(-30, 30), colorbar_title="$(flabels[f])");
-        Plots.plot!(size=(1000, 900));
-        Plots.xlabel!("α");
-        Plots.ylabel!("β");
+                   st=:surface, title=title, camera=(-30, 30),
+                   colorbar_title="$(flabels[f])", titlefontsize=21,
+                   xlabel="α", xtickfontsize=18, xguidefontsize=22,
+                   ylabel="β", ytickfontsize=18, yguidefontsize=22,
+                   ztickfontsize=12, colorbar_titlefontsize=14, colorbar_tickfontsize=12,
+                   size=(1000, 900));
         Plots.savefig("$(FIGURES_FOLDER)$(flabels[f])_sfplt_$i.pdf")
     end
 end
--- _120_fit_likelihood.jl
+++ _120_fit_likelihood.jl
@@ -6,59 +6,88 @@ import Distributions: Beta, pdf
 import Random: seed! 
 
 using Optim
+using Plots
 
+const FIGURES_FOLDER = "./_900_output/figures/"
 const N = 100; # True population size
 const n = 25; # Number of replications
 const T = 2;
 # Matrix of true alpha and beta parameters for simulation
-const PARAMS = [4.0 1.0];
+const PARAMS = [1.0 4.0; 1.0 1.0; 4.0 1.0];
 
 
-d = Beta(PARAMS[1], PARAMS[2]);
-p = rand(d, N);
-estis = [];
-for i in 1:100
-    K = simulate_caprecap(N, n, T, p);
-    function g!(G, x)
-        firstprod = 1.0;
-        secondprod = 1.0;
-        firstsum = 0.0;
-        secondsum = 0.0;
-        thirdsum = 0.0;
-        fourthsum = 0.0;
-        fifthsum = 0.0;
-        for j in 1:T
-            firstprod *= ( 1.0 - (j - x[1]) / (x[2] + T) );
-            secondprod *= ( 1.0 - j / (x[2] + T) );
-            firstsum += ( 1.0 - (j - x[1]) / (x[2] + T) )^(-1);
-            secondsum += (j - x[1]) * ( 1.0 - (j - x[1]) / (x[2] + T) )^(-1);
-            thirdsum += j * ( 1.0 - (j - x[1]) / (x[2] + T) )^(-1);
-        end
-        for k in values(K)
-            firstinnersum = 0.0;
-            secondinnersum = 0.0;
-            for j in 1:k
-                firstinnersum += ( j / (x[1] + k - j) );
+seed!(555);
+for row in 1:size(PARAMS)[1]
+    alpha = PARAMS[row, 1];
+    beta = PARAMS[row, 2]
+    d = Beta(alpha, beta);
+    p = rand(d, N);
+    estis = [];
+    for i in 1:100
+        K = simulate_caprecap(N, n, T, p);
+        function g!(G, x)
+            firstprod = 1.0;
+            secondprod = 1.0;
+            firstsum = 0.0;
+            secondsum = 0.0;
+            thirdsum = 0.0;
+            fourthsum = 0.0;
+            fifthsum = 0.0;
+            for j in 1:T
+                firstprod *= ( 1.0 - (j - x[1]) / (x[2] + T) );
+                secondprod *= ( 1.0 - j / (x[2] + T) );
+                firstsum += ( 1.0 - (j - x[1]) / (x[2] + T) )^(-1);
+                secondsum += (j - x[1]) * ( 1.0 - (j - x[1]) / (x[2] + T) )^(-1);
+                thirdsum += j * ( 1.0 - (j - x[1]) / (x[2] + T) )^(-1);
             end
-            for j in 1:(T - k)
-                secondinnersum += ( (k - j) / (x[2] + T - k + j) );
+            for k in values(K)
+                firstinnersum = 0.0;
+                secondinnersum = 0.0;
+                for j in 1:k
+                    firstinnersum += ( j / (x[1] + k - j) );
+                end
+                for j in 1:(T - k)
+                    secondinnersum += ( (k - j) / (x[2] + T - k + j) );
+                end
+                fourthsum += (x[1] + k)^(-1) * (k - firstinnersum);
+                fifthsum += k - secondinnersum;
             end
-            fourthsum += (x[1] + k)^(-1) * (k - firstinnersum);
-            fifthsum += k - secondinnersum;
+            G[1] = -( -length(K) / (x[2] + T)
+                    * firstprod * firstsum
+                    / (firstprod - secondprod) + fourthsum );
+            G[2] = -( -length(K) / ((x[2] + T)^2)
+                    * ((firstprod * secondsum - secondprod * thirdsum)
+                    / (firstprod - secondprod)) - (x[2] + T)^(-1) * fifthsum ); 
         end
-        G[1] = -( -length(K) / (x[2] + T)
-                * firstprod * firstsum
-                / (firstprod - secondprod) + fourthsum );
-        G[2] = -( -length(K) / ((x[2] + T)^2)
-                * ((firstprod * secondsum - secondprod * thirdsum)
-                / (firstprod - secondprod)) - (x[2] + T)^(-1) * fifthsum ); 
+        L(x) = -loglikelihood(x[1], x[2], K, T);
+        lower = [0.01, 0.01]
+        upper = [Inf, Inf]
+        initial_x = [2.0, 2.0]
+        inner_optimizer = GradientDescent()
+        results = optimize(L, g!, lower, upper, initial_x, Fminbox(inner_optimizer));
+        push!(estis, Optim.minimizer(results));
+        println("$row, $i");
     end
-    L(x) = -loglikelihood(x[1], x[2], K, T);
-    lower = [0.01, 0.01]
-    upper = [Inf, Inf]
-    initial_x = [5.0, 5.0]
-    inner_optimizer = GradientDescent()
-    results = optimize(L, g!, lower, upper, initial_x, Fminbox(inner_optimizer));
-    push!(estis, Optim.minimizer(results));
-    println("Yes");
+    
+    histogram([i[1] for i in estis],
+              xlabel="α_hat", label=false,
+              size=(1200, 900), xtickfontsize=22,
+              xguidefontsize=24, ytickfontsize=22);
+    vline!([alpha],
+           label="True α = $alpha",
+           linewidth=12,
+           legendfontsize=22);
+    savefig("$(FIGURES_FOLDER)hist_alpha_$row.pdf");
+    histogram([i[2] for i in estis],
+              xlabel="β_hat", label=false,
+              size=(1200, 900), xtickfontsize=22,
+              xguidefontsize=24, ytickfontsize=22,
+              color="purple");
+    vline!([beta],
+           label="True β = $beta",
+           linewidth=12,
+           legendfontsize=22,
+           color="gold");
+    savefig("$(FIGURES_FOLDER)hist_beta_$row.pdf");
 end
+
