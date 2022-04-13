364acb2502d4148cf286faf1940c2a4ca16caa0b
Nurzhan Sapargali <nurzhan@nurzh-pc.nurzh-pc>
2022-04-13 16:41:37 +0200

    Some addition to integration

--- _100_utils.jl
+++ _100_utils.jl
@@ -8,8 +8,8 @@ export simulate_caprecap
 function numerator_integrand(p, alpha, N_u, N_o, S, unit)
     x_T = [unit in s_t for s_t in S];
     y = ( p^(alpha + length(S) - 1.0)
-          * (1.0 - p)^(alpha * (N_o + N_u - 1.0) - 1.0)
-          * prod([1.0 / (length(s_t) * p) - 1 for s_t in S] .^ (1 .- x_T)) );
+          * (1 - p)^(alpha * (N_o + N_u - 1.0) - 1.0)
+          * prod([1 / (length(s_t) * p) - 1 for s_t in S] .^ (1 .- x_T)) );
     return y;
 end
 
@@ -20,21 +20,30 @@ function denominator_integrand(p, alpha, N_u, N_o, S)
     return y;
 end
 
-function trapezoid(f, a, b, grid)
+function trapezoid(f, grid)
     integral = 0.0;
     for k in 2:length(grid)
          integral += ( f(grid[k]) - f(grid[k-1]) ) * (grid[k] - grid[k-1]);
     end
     return integral * 0.5;
 end
-        
+
+function monte_carlo(alpha, N_u, N_o, S, sample_size, unit)
+    n = [length(i) for i in S];
+    d = Beta(alpha, alpha * (N_o + N_u - 1));
+    p_star = rand(d, sample_size);
+    x = [unit in i for i in S];
+    dense = [prod( (k * n).^x .* (1 .- n .* k).^(1 .- x) ) for k in p_star];
+    return sum(dense) / length(dense)
+end
+
 function likelihood(alpha, N_u, S)
     O = Set([i for j in S for i in j]);
     N_o = length(O);
     product_term = 1.0;
     for i in O
         f(x) = numerator_integrand(x, alpha, N_u, N_o, S, i);
-        product_term *= trapezoid(f, 0, 1, 0.02:0.01:0.99);
+        product_term *= trapezoid(f, 0.02:0.01:0.99);
     end
     return beta(alpha, alpha * (N_u + N_o - 1))^(-N_o) * product_term;
 end
@@ -45,13 +54,30 @@ function loglikelihood(alpha, N_u, S)
     sum_term = 0.0;
     for i in O
         f(x) = numerator_integrand(x, alpha, N_u, N_o, S, i);
-        I = trapezoid(f, 0, 1, 0.02:0.01:0.99);
+        I = trapezoid(f, 0.02:0.01:0.99);
         if I <= 0
-            I = 5.0e-324;
+            I = 4.9406564584124654e-324;
         end
         I = log(I);
         sum_term += I; 
     end
     g(x) = denominator_integrand(x, alpha, N_u, N_o, S);
     return -N_o * logbeta(alpha, alpha * (N_u + N_o - 1)) + sum_term;
+end
+
+function loglikelihood_truncated(alpha, N_u, S)
+    O = Set([i for j in S for i in j]);
+    N_o = length(O);
+    sum_term = 0.0;
+    for i in O
+        f(x) = numerator_integrand(x, alpha, N_u, N_o, S, i);
+        I = trapezoid(f, 0.02:0.01:0.99);
+        if I <= 0
+            I = 4.9406564584124654e-324;
+        end
+        I = log(I);
+        sum_term += I; 
+    end
+    g(x) = denominator_integrand(x, alpha, N_u, N_o, S);
+    return -N_o * log(beta(alpha, alpha * (N_u + N_o - 1)) - trapezoid(g, 0.02:0.01:0.99)) + sum_term;
 end
--- _110_simulate_data.jl
+++ _110_simulate_data.jl
@@ -3,7 +3,7 @@ using SpecialFunctions
 using Optim
 using StatsBase
 
-ALPHA = 0.1;
+ALPHA = 10;
 N = 1000;
 n = [50; 50; 100; 40; 30; 30; 100; 50; 20; 30; 5; 10; 25; 35; 25; 10; 10; 25; 10; 5; 50; 50; 100; 40; 30; 30; 100; 50; 20; 30; 5; 10; 25; 35; 25; 10; 10; 25; 10; 5]
 
@@ -13,4 +13,4 @@ p = rand(d, N);
 S = [sample(1:N, ProbabilityWeights(p), i, replace=false) for i in n]
 LL(alpha, N_u) = loglikelihood(alpha, N_u, S);
 O = Set([i for j in S for i in j]);
-contourf(0.1:0.2:2.1, 2:100:2000, LL)
+contourf(1.0:20.0, 2:100:2000, LL)
