48dec37557cf1d23ce60f54e30d00a321e715459
Nurzhan Sapargali <nurzhan@nurzh-pc.nurzh-pc>
2022-04-19 17:22:39 +0200

    Change likelihood functions

--- _100_utils.jl
+++ _100_utils.jl
@@ -1,67 +1,54 @@
 module Utils
 
-import SpecialFunctions: beta
+import Distributions: Beta
 
-export simulate_caprecap
+export loglh, loglh_truncated
 
 
-function numerator_integrand(p, alpha, N_u, N_o, S, unit)
-    x_T = [unit in s_t for s_t in S];
-    y = ( p^(alpha + length(S) - 1.0)
-          * (1 - p)^(alpha * (N_o + N_u - 1.0) - 1.0)
-          * prod([1 / (length(s_t) * p) - 1 for s_t in S] .^ (1 .- x_T)) );
-    return y;
+function monte_carlo(alpha, beta, x_i, n, mcdraws)
+    sum_term = 0.0;
+    points = rand(Beta(alpha, beta), mcdraws);
+    for p in points
+        g = ((n .* p).^(-1.0) .- 1).^(1 .- x_i);
+        sum_term += prod(g);
+    end
+    return sum_term / mcdraws;
 end
 
-function denominator_integrand(p, alpha, N_u, N_o, S)
-    y = ( p^(alpha - 1.0)
-          * (1.0 - p)^(alpha * (N_o + N_u - 1.0) - 1.0)
-          * prod([1.0 - length(s_t) * p for s_t in S]) );
-    return y;
-end
-
-function monte_carlo(alpha, N_u, N_o, S, sample_size, unit)
-    n = [length(i) for i in S];
-    d = Beta(alpha + length(n), alpha * (N_o + N_u - 1));
-    p_star = rand(d, sample_size);
-    x = [unit in i for i in S];
-    dense = [prod( (1 ./ (n .* k) .- 1).^(1 .- x) ) for k in p_star];
-    return sum(dense) / length(dense)
-end
-
-function loglikelihood(alpha, N_u, S, sample_size, truncated=false)
+function loglh(alpha, N_u, S, mcdraws)
     O = Set([i for j in S for i in j]);
     N_o = length(O);
     T = length(S);
     n = [length(i) for i in S];
-    sum_term = 0.0;
+    first_sum_term = 0.0;
+    second_sum_term = 0.0;
+    for t in 1:T
+        first_sum_term += log(alpha + T - t) - log(alpha * (N_o + N_u) + T - t);
+    end
     for i in O
-        I = monte_carlo(alpha, N_u, N_o, S, sample_size, i);
-        sum_term += log(I);
+        x_i = [i in s for s in S]; 
+        I = monte_carlo(alpha + T, alpha * (N_o + N_u - 1), x_i, n, mcdraws);
+        second_sum_term += log(I);
     end
-    if truncated
-        product_term = prod([n[t] * (alpha * (N_o + N_u) + T - t) / (alpha + T - t) for t in 1:T]);
-        return ( -N_o * (log(product_term - monte_carlo(alpha, N_u, N_o, S, sample_size, -1)))
-                 + sum_term );
-    else
-        return ( N_o * sum([log(alpha + T - t) - log(alpha * (N_o + N_u) + T - t) for t in 1:T])
-                + sum_term );
-    end
+    return N_o * first_sum_term + second_sum_term;
 end
 
-function loglikelihood_truncated(alpha, N_u, S)
+function loglh_truncated(alpha, N_u, S, mcdraws)
     O = Set([i for j in S for i in j]);
     N_o = length(O);
+    T = length(S);
+    n = [length(i) for i in S];
+    product_term = 1.0;
     sum_term = 0.0;
-    for i in O
-        f(x) = numerator_integrand(x, alpha, N_u, N_o, S, i);
-        I = trapezoid(f, 0.02:0.01:0.99);
-        if I <= 0
-            I = 4.9406564584124654e-324;
-        end
-        I = log(I);
-        sum_term += I; 
+    for t in 1:T
+        product_term *= (alpha * (N_u + N_o) + T - t) / (alpha + T - t);
     end
-    g(x) = denominator_integrand(x, alpha, N_u, N_o, S);
-    return -N_o * log(beta(alpha, alpha * (N_u + N_o - 1)) - trapezoid(g, 0.02:0.01:0.99)) + sum_term;
+    product_term *= prod(n);
+    product_term -= monte_carlo(alpha + T, alpha * (N_u + N_o - 1), zeros(T), n, mcdraws);
+    for i in O
+        x_i = [i in s for s in S];
+        I = monte_carlo(alpha + T, alpha * (N_o + N_u - 1), x_i, n, mcdraws);
+        sum_term += log(I);
+    end
+    return N_o * log(product_term) + sum_term;
 end
--- _110_simulate_data.jl
+++ _110_simulate_data.jl
@@ -1,9 +1,11 @@
+using .Utils
+
 using Distributions
 using SpecialFunctions
-using Optim
 using StatsBase
+using Plots
 
-ALPHA = 10;
+ALPHA = 2;
 N = 1000;
 n = [50; 50; 100; 40; 30; 30; 100]
 
@@ -11,6 +13,6 @@ n = [50; 50; 100; 40; 30; 30; 100]
 d = Beta(ALPHA, ALPHA * (N - 1));
 p = rand(d, N);
 S = [sample(1:N, ProbabilityWeights(p), i, replace=false) for i in n]
-LL(alpha, N_u) = loglikelihood(alpha, N_u, S, 1000);
+LL(alpha, N_u) = loglh_truncated(alpha, N_u, S, 1000);
 O = Set([i for j in S for i in j]);
 contourf(1.0:20.0, 2:100:2000, LL)
