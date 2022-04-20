ab4a69ad879b42c61ecbee7b3c5450bf55026fc6
Nurzhan Sapargali <nurzhan@nurzh-pc.nurzh-pc>
2022-04-20 17:49:52 +0200

    Set up optimiser, fix likelihood, integration

--- _100_utils.jl
+++ _100_utils.jl
@@ -9,7 +9,7 @@ function monte_carlo(alpha, beta, x_i, n, mcdraws)
     sum_term = 0.0;
     points = rand(Beta(alpha, beta), mcdraws);
     for p in points
-        g = ((n .* p).^(-1.0) .- 1).^(1 .- x_i);
+        g = (n .* p).^x_i .* (1 .- n .* p).^(1 .- x_i)
         sum_term += prod(g);
     end
     return sum_term / mcdraws;
@@ -20,17 +20,17 @@ function loglh(alpha, N_u, S, mcdraws)
     N_o = length(O);
     T = length(S);
     n = [length(i) for i in S];
-    first_sum_term = 0.0;
-    second_sum_term = 0.0;
-    for t in 1:T
-        first_sum_term += log(alpha + T - t) - log(alpha * (N_o + N_u) + T - t);
-    end
+    sum_term = 0.0;
     for i in O
         x_i = [i in s for s in S]; 
-        I = monte_carlo(alpha + T, alpha * (N_o + N_u - 1), x_i, n, mcdraws);
-        second_sum_term += log(I);
+        I = monte_carlo(alpha, alpha * (N_o + N_u - 1), x_i, n, mcdraws);
+        if I < 0
+            I = 5e-200;
+        end
+        sum_term += log(I);
     end
-    return N_o * first_sum_term + second_sum_term;
+    println("alpha = $alpha, N_u = $N_u, lh = $sum_term");
+    return sum_term;
 end
 
 function loglh_truncated(alpha, N_u, S, mcdraws)
@@ -38,17 +38,11 @@ function loglh_truncated(alpha, N_u, S, mcdraws)
     N_o = length(O);
     T = length(S);
     n = [length(i) for i in S];
-    product_term = 1.0;
-    sum_term = 0.0;
-    for t in 1:T
-        product_term *= (alpha * (N_u + N_o) + T - t) / (alpha + T - t);
+    denom = 1 - monte_carlo(alpha, alpha * (N_u + N_o - 1), zeros(T), n, mcdraws);
+    if denom < 0
+        denom = 5e-200
     end
-    product_term *= prod(n);
-    product_term -= monte_carlo(alpha + T, alpha * (N_u + N_o - 1), zeros(T), n, mcdraws);
-    for i in O
-        x_i = [i in s for s in S];
-        I = monte_carlo(alpha + T, alpha * (N_o + N_u - 1), x_i, n, mcdraws);
-        sum_term += log(I);
-    end
-    return N_o * log(product_term) + sum_term;
+    lh = -N_o * log(denom) + loglh(alpha, N_u, S, mcdraws)
+    println("alpha = $alpha, N_u = $N_u, lh = $lh");
+    return lh;
 end
--- _110_simulate_data.jl
+++ _110_simulate_data.jl
@@ -5,14 +5,22 @@ using SpecialFunctions
 using StatsBase
 using Plots
 
-ALPHA = 2;
-N = 1000;
-n = [50; 50; 100; 40; 30; 30; 100]
+ALPHA = 1;
+N = 10000;
+T = 2;
+AVG_SAMPLE_SIZE = 250;
 
-
+n = rand(Poisson(AVG_SAMPLE_SIZE), T)
 d = Beta(ALPHA, ALPHA * (N - 1));
 p = rand(d, N);
 S = [sample(1:N, ProbabilityWeights(p), i, replace=false) for i in n]
-LL(alpha, N_u) = loglh_truncated(alpha, N_u, S, 1000);
 O = Set([i for j in S for i in j]);
-contourf(1.0:20.0, 2:100:2000, LL)
+LL(alpha, N_u) = -loglh_truncated(alpha, N_u, S, 5000);
+xs = [];
+lower = [0.1, 2];
+upper = [Inf, Inf];
+cbk = tr -> begin
+            push!(xs, tr[end].metadata["centroid"])
+            false
+            end
+res = optimize(LL, lower, upper, [5.0, 10.0], Fminbox(NelderMead()), Optim.Options(store_trace=true, extended_trace=true, iterations=3, callback=cbk));
