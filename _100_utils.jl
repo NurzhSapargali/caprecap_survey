cd8c2d224fccf6850eecacaaf869cf13d9984d36
Nurzhan Sapargali <nurzhan@nurzh-pc.nurzh-pc>
2022-04-22 16:25:53 +0200

    Change optimisers

--- _100_utils.jl
+++ _100_utils.jl
@@ -1,21 +1,44 @@
 module Utils
 
 import Distributions: Beta
+import SpecialFunctions: beta
 
 export loglh, loglh_truncated
 
 
-function monte_carlo(alpha, beta, x_i, n, mcdraws)
+function f(alpha_parameter, beta_parameter, 
+integrand = ( prod(g) * p^(alpha_parameter - 1.0)
+                      * (1.0 - p)^(beta_parameter - 1.0)
+                      / beta(alpha_parameter, beta_parameter) );
+function monte_carlo(alpha_parameter, beta_parameter, x_i, n, mcdraws)
     sum_term = 0.0;
-    points = rand(Beta(alpha, beta), mcdraws);
+    points = rand(Beta(alpha_parameter, beta_parameter), mcdraws);
     for p in points
-        g = (n .* p).^x_i .* (1 .- n .* p).^(1 .- x_i)
+        g = (n .* p).^x_i .* (1.0 .- n .* p).^(1.0 .- x_i);
         sum_term += prod(g);
     end
     return sum_term / mcdraws;
 end
 
-function loglh(alpha, N_u, S, mcdraws)
+function trapezoid(alpha_parameter, beta_parameter, x_i, n, draws, a, b)
+    delta = (b - a) / draws;
+    points = a:delta:b;
+    sum_term = 0.0;
+    for p in points
+        g = (n .* p).^x_i .* (1.0 .- n .* p).^(1.0 .- x_i);
+        integrand = ( prod(g) * p^(alpha_parameter - 1.0)
+                      * (1.0 - p)^(beta_parameter - 1.0)
+                      / beta(alpha_parameter, beta_parameter) );
+        if (p == a || p == b)
+            sum_term += integrand;
+        else
+            sum_term += 2.0 * integrand;
+        end
+    end
+    return delta / 2.0 * sum_term;
+end
+
+function loglh(alpha_parameter, N_u, S, draws)
     O = Set([i for j in S for i in j]);
     N_o = length(O);
     T = length(S);
@@ -23,26 +46,28 @@ function loglh(alpha, N_u, S, mcdraws)
     sum_term = 0.0;
     for i in O
         x_i = [i in s for s in S]; 
-        I = monte_carlo(alpha, alpha * (N_o + N_u - 1), x_i, n, mcdraws);
+        I = monte_carlo(alpha_parameter, alpha_parameter * (N_o + N_u - 1), x_i, n, draws);
+        #I = trapezoid(alpha_parameter, alpha_parameter * (N_o + N_u - 1), x_i, n, draws, 0.0, 1.0);
         if I < 0
             I = 5e-200;
         end
         sum_term += log(I);
     end
-    println("alpha = $alpha, N_u = $N_u, lh = $sum_term");
+    #println("alpha_parameter = $alpha_parameter, N_u = $N_u, lh = $sum_term");
     return sum_term;
 end
 
-function loglh_truncated(alpha, N_u, S, mcdraws)
+function loglh_truncated(alpha_parameter, N_u, S, draws)
     O = Set([i for j in S for i in j]);
     N_o = length(O);
     T = length(S);
     n = [length(i) for i in S];
-    denom = 1 - monte_carlo(alpha, alpha * (N_u + N_o - 1), zeros(T), n, mcdraws);
+    denom = 1 - monte_carlo(alpha_parameter, alpha_parameter * (N_u + N_o - 1), zeros(T), n, draws);
+    #denom = 1 - trapezoid(alpha_parameter, alpha_parameter * (N_u + N_o - 1), zeros(T), n, draws, 0.0, 1.0);
     if denom < 0
         denom = 5e-200
     end
-    lh = -N_o * log(denom) + loglh(alpha, N_u, S, mcdraws)
-    println("alpha = $alpha, N_u = $N_u, lh = $lh");
+    lh = -N_o * log(denom) + loglh(alpha_parameter, N_u, S, draws)
+    println("alpha_parameter = $alpha_parameter, N_u = $N_u, lh = $lh");
     return lh;
 end
--- _110_simulate_data.jl
+++ _110_simulate_data.jl
@@ -3,24 +3,22 @@ using .Utils
 using Distributions
 using SpecialFunctions
 using StatsBase
-using Plots
+using NLopt
 
 ALPHA = 1;
 N = 10000;
 T = 2;
 AVG_SAMPLE_SIZE = 250;
 
-n = rand(Poisson(AVG_SAMPLE_SIZE), T)
+n = rand(Poisson(AVG_SAMPLE_SIZE), T);
 d = Beta(ALPHA, ALPHA * (N - 1));
 p = rand(d, N);
-S = [sample(1:N, ProbabilityWeights(p), i, replace=false) for i in n]
+S = [sample(1:N, ProbabilityWeights(p), i, replace=false) for i in n];
 O = Set([i for j in S for i in j]);
-LL(alpha, N_u) = -loglh_truncated(alpha, N_u, S, 5000);
-xs = [];
+LL(x, grad) = -loglh_truncated(x[1], x[2], S, 100000);
+opt = Opt(:LN_SBPLX, 2);
 lower = [0.1, 2];
-upper = [Inf, Inf];
-cbk = tr -> begin
-            push!(xs, tr[end].metadata["centroid"])
-            false
-            end
-res = optimize(LL, lower, upper, [5.0, 10.0], Fminbox(NelderMead()), Optim.Options(store_trace=true, extended_trace=true, iterations=3, callback=cbk));
+opt.lower_bounds = lower;
+opt.ftol_abs = 0.01;
+opt.min_objective = LL;
+(minf, minx, ret) = NLopt.optimize(opt, [5.0, 10.0]);
