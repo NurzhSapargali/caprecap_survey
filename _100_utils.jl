02ab1fe986af1c009e8f07e5a20609baf46163a4
Nurzhan Sapargali <nurzhan.sapargali@stat.uni-muenchen.de>
2022-04-26 05:43:57 +0600

    Add Lincoln and Schnabel estimators, trials

--- _100_utils.jl
+++ _100_utils.jl
@@ -1,14 +1,14 @@
 module Utils
 
 import Distributions: Beta
-import SpecialFunctions: beta
+import StatsBase: addcounts!
 
 export loglh, loglh_truncated
 
 
-function monte_carlo(alpha_parameter, beta_parameter, x_i, n, mcdraws)
+function monte_carlo(alpha, beta, x_i, n, mcdraws)
     sum_term = 0.0;
-    points = rand(Beta(alpha_parameter, beta_parameter), mcdraws);
+    points = rand(Beta(alpha, beta), mcdraws);
     for p in points
         g = (n .* p).^x_i .* (1.0 .- n .* p).^(1.0 .- x_i);
         sum_term += prod(g);
@@ -16,25 +16,22 @@ function monte_carlo(alpha_parameter, beta_parameter, x_i, n, mcdraws)
     return sum_term / mcdraws;
 end
 
-function trapezoid(alpha_parameter, beta_parameter, x_i, n, draws, a, b)
-    delta = (b - a) / draws;
-    points = a:delta:b;
-    sum_term = 0.0;
-    for p in points
-        g = (n .* p).^x_i .* (1.0 .- n .* p).^(1.0 .- x_i);
-        integrand = ( prod(g) * p^(alpha_parameter - 1.0)
-                      * (1.0 - p)^(beta_parameter - 1.0)
-                      / beta(alpha_parameter, beta_parameter) );
-        if (p == a || p == b)
-            sum_term += integrand;
-        else
-            sum_term += 2.0 * integrand;
-        end
-    end
-    return delta / 2.0 * sum_term;
+function lincoln(S)
+    r = intersect(Set(S[1]), Set(S[2]));
+    return prod([length(s) for s in S]) / length(r);
 end
 
-function loglh(alpha_parameter, N_u, S, draws)
+function schnabel(S)
+    K = Dict{Int, Int}();
+    for s in S
+        addcounts!(K, s);
+    end
+    n = [length(s) for s in S];
+    T = length(S);
+    return sum(n[2:T] .* cumsum(n)[2:T]) / sum(values(K) .- 1);
+end
+
+function loglh(alpha, N_u, S, draws)
     O = Set([i for j in S for i in j]);
     N_o = length(O);
     T = length(S);
@@ -42,28 +39,25 @@ function loglh(alpha_parameter, N_u, S, draws)
     sum_term = 0.0;
     for i in O
         x_i = [i in s for s in S]; 
-        I = monte_carlo(alpha_parameter, alpha_parameter * (N_o + N_u - 1), x_i, n, draws);
-        #I = trapezoid(alpha_parameter, alpha_parameter * (N_o + N_u - 1), x_i, n, draws, 0.0, 1.0);
+        I = monte_carlo(alpha, alpha * (N_o + N_u - 1), x_i, n, draws);
         if I < 0
             I = 5e-200;
         end
         sum_term += log(I);
     end
-    #println("alpha_parameter = $alpha_parameter, N_u = $N_u, lh = $sum_term");
     return sum_term;
 end
 
-function loglh_truncated(alpha_parameter, N_u, S, draws)
+function loglh_truncated(alpha, N_u, S, draws)
     O = Set([i for j in S for i in j]);
     N_o = length(O);
     T = length(S);
     n = [length(i) for i in S];
-    denom = 1 - monte_carlo(alpha_parameter, alpha_parameter * (N_u + N_o - 1), zeros(T), n, draws);
-    #denom = 1 - trapezoid(alpha_parameter, alpha_parameter * (N_u + N_o - 1), zeros(T), n, draws, 0.0, 1.0);
+    denom = 1 - monte_carlo(alpha, alpha * (N_u + N_o - 1), zeros(T), n, draws);
     if denom < 0
         denom = 5e-200
     end
-    lh = -N_o * log(denom) + loglh(alpha_parameter, N_u, S, draws)
-    println("alpha_parameter = $alpha_parameter, N_u = $N_u, lh = $lh");
+    lh = -N_o * log(denom) + loglh(alpha, N_u, S, draws)
+    println("alpha = $alpha, N_u = $N_u, lh = $lh");
     return lh;
 end
--- _110_simulate_data.jl
+++ _110_simulate_data.jl
@@ -5,20 +5,27 @@ using SpecialFunctions
 using StatsBase
 using NLopt
 
-ALPHA = 1;
-N = 10000;
+ALPHA = 0.001;
+N = 1000;
 T = 2;
-AVG_SAMPLE_SIZE = 250;
+AVG_SAMPLE_SIZE = 25;
 
-n = rand(Poisson(AVG_SAMPLE_SIZE), T);
-d = Beta(ALPHA, ALPHA * (N - 1));
-p = rand(d, N);
-S = [sample(1:N, ProbabilityWeights(p), i, replace=false) for i in n];
-O = Set([i for j in S for i in j]);
-LL(x, grad) = -loglh_truncated(x[1], x[2], S, 100000);
-opt = Opt(:LN_SBPLX, 2);
-lower = [0.1, 2];
-opt.lower_bounds = lower;
-opt.ftol_abs = 0.01;
-opt.min_objective = LL;
-(minf, minx, ret) = NLopt.optimize(opt, [5.0, 10.0]);
+estis = [];
+links = [];
+for trial in 1:20
+    println("***TRIAL NO $trial***")
+    n = rand(Poisson(AVG_SAMPLE_SIZE), T);
+    d = Beta(ALPHA, ALPHA * (N - 1));
+    p = rand(d, N);
+    S = [sample(1:N, ProbabilityWeights(p), i, replace=false) for i in n];
+    O = Set([i for j in S for i in j]);
+    LL(x, grad) = -loglh_truncated(x[1], x[2], S, 100000);
+    opt = Opt(:LN_SBPLX, 2);
+    lower = [0.1, 2];
+    opt.lower_bounds = lower;
+    opt.min_objective = LL;
+    opt.xtol_abs = 0.1;
+    (minf, minx, ret) = NLopt.optimize(opt, [5.0, 10.0]);
+    push!(estis, round(minx[2]) + length(O));
+    push!(links, round(lincoln(S)));
+end
