178d86a0c09fcccf202be95eb5875cbeeb1a3d9e
Nurzhan Sapargali <nurzhan.sapargali@stat.uni-muenchen.de>
2022-04-28 04:58:24 +0600

    Adds jackknife estimator, refactor simulation code

--- _100_utils.jl
+++ _100_utils.jl
@@ -25,48 +25,59 @@ function monte_carlo(alpha, beta, x_i, n, mcdraws)
     return sum_term / mcdraws;
 end
 
-function lincoln(S)
+function lincoln(S, n)
     r = intersect(Set(S[1]), Set(S[2]));
-    return prod([length(s) for s in S]) / length(r);
+    return prod(n) / length(r);
 end
 
-function schnabel(S)
-    K = Dict{Float64, Int}();
-    for s in S
-        addcounts!(K, s);
-    end
-    n = [length(s) for s in S];
-    T = length(S);
+function schnabel(n, T, K)
     return sum(n[2:T] .* cumsum(n)[2:T]) / sum(values(K) .- 1.0);
 end
 
-function chao(S)
-    K = Dict{Float64, Int}();
-    for s in S
-        addcounts!(K, s);
-    end
-    f = countmap(values(K));
-    O = Set([i for j in S for i in j]);
-    return length(O) + f[1]^2.0 / (2.0 * f[2]);
+function chao(N_o, f)
+    return N_o + get(f, 1, 0)^2.0 / (2.0 * get(f, 2, 0));
 end
 
-function chao_corrected(S)
-    K = Dict{Float64, Int}();
-    for s in S
-        addcounts!(K, s);
-    end
-    f = countmap(values(K));
-    O = Set([i for j in S for i in j]);
-    m1 = 2.0 * f[2] / f[1];
-    m2 = 6.0 * f[3] / f[1];
-    return length(O) + f[1]^2.0 / (2.0 * f[2]) * (1.0 - m1 / length(S)) / (1.0 - m2 / (length(S) * m1));
+function chao_corrected(N_o, T, f)
+    m1 = 2.0 * get(f, 2, 0)  / get(f, 1, 0);
+    m2 = 6.0 * get(f, 3, 0) / get(f, 1, 0);
+    return (N_o
+            + get(f, 1, 0)^2.0 / (2.0 * get(f, 2, 0))
+            * (1.0 - m1 / T) / (1.0 - m2 / (T * m1)));
 end
 
-function loglh(alpha, N_u, S, draws)
-    O = Set([i for j in S for i in j]);
+function jackknife(N_o, T, f, k)
+    if k == 1
+        return N_o + (T - 1.0) / T * get(f, 1, 0);
+    elseif k == 2
+        return (N_o
+                + (2.0 * T - 3.0) / T * get(f, 1, 0)
+                - (T - 2.0)^2.0 / (T^2.0 - T) * get(f, 2, 0));
+    elseif k == 3
+        return (N_o
+                + (3.0 * T - 6.0) / T * get(f, 1, 0)
+                - (3.0 * T^2.0 - 15.0 * T + 19.0) / (T^2.0 - T) * get(f, 2, 0)
+                + (T - 3.0)^3.0 / (T * (T - 1.0) * (T - 2.0)) * get(f, 3, 0));
+    elseif k == 4
+        return (N_o
+                + (4.0 * T - 10.0) / T * get(f, 1, 0)
+                - (6.0 * T^2.0 - 36.0 * T + 55.0) / (T^2.0 - T) * get(f, 2, 0)
+                + (4.0 * T^3.0 - 42.0 * T^2.0 + 148.0 * T - 175.0) / (T * (T - 1.0) * (T - 2.0)) * get(f, 3, 0)
+                - (T - 4.0)^4.0 / (T * (T - 1.0) * (T - 2.0) * (T - 3.0)) * get(f, 4, 0));
+    elseif k == 5
+        return (N_o
+                + (5.0 * T - 15.0) / T * get(f, 1, 0)
+                - (10.0 * T^2.0 - 70.0 * T + 125.0) / (T^2.0 - T) * get(f, 2, 0)
+                + (10.0 * T^3.0 - 120.0 * T^2.0 + 485.0 * T - 660.0) / (T * (T - 1.0) * (T - 2.0)) * get(f, 3, 0)
+                - ((T - 4.0)^5.0 - (T - 5.0)^5.0) / (T * (T - 1.0) * (T - 2.0) * (T - 3.0)) * get(f, 4, 0)
+                + (T - 5.0)^5.0 / (T * (T - 1.0) * (T - 2.0) * (T - 3.0) * (T - 4.0)) * get(f, 5, 0));
+    else
+        error("k must be between 1 and 5");
+    end
+end
+
+function loglh(alpha, N_u, S, O, T, n, draws)
     N_o = length(O);
-    T = length(S);
-    n = [length(i) for i in S];
     sum_term = 0.0;
     for i in O
         x_i = [i in s for s in S]; 
@@ -79,16 +90,13 @@ function loglh(alpha, N_u, S, draws)
     return sum_term;
 end
 
-function loglh_truncated(alpha, N_u, S, draws)
-    O = Set([i for j in S for i in j]);
+function loglh_truncated(alpha, N_u, S, O, T, n, draws)
     N_o = length(O);
-    T = length(S);
-    n = [length(i) for i in S];
     denom = 1 - monte_carlo(alpha, alpha * (N_u + N_o - 1), zeros(T), n, draws);
     if denom < 0
         denom = 5e-200
     end
-    lh = -N_o * log(denom) + loglh(alpha, N_u, S, draws)
+    lh = -N_o * log(denom) + loglh(alpha, N_u, S, O, T, n, draws)
     println("alpha = $alpha, N_u = $N_u, lh = $lh");
     return lh;
 end
--- _110_simulate_data.jl
+++ _110_simulate_data.jl
@@ -7,37 +7,49 @@ using NLopt
 
 import StatsBase: efraimidis_a_wsample_norep!
 
-ALPHAS = [0.1];
+ALPHAS = [0.1, 0.5, 1.0, 5.0, 20.0];
 N = 1000;
-T = [5];
+T = [2, 5, 8, 10];
 AVG_SAMPLE_SIZE = 50.0;
 
 
+    
 estis = [];
 links = [];
 schnab = [];
+chaos = [];
+chaos_corr = [];
+jks = [];
 for alpha in ALPHAS
     d = Beta(alpha, alpha * (N - 1));
     p = rand(d, N);
     for t in T
         n = rand(Poisson(AVG_SAMPLE_SIZE), t);
-        for trial in 1:100
+        for trial in 1:1
             println("***TRIAL NO $trial, $alpha, $t***")
             S = [efraimidis_a_wsample_norep!(1:N, Weights(p), zeros(i)) for i in n];
             O = Set([i for j in S for i in j]);
-            LL(x, grad) = -loglh_truncated(x[1], x[2], S, 1000);
+            K = Dict{Float64, Int}();
+            for s in S
+                addcounts!(K, s);
+            end
+            f = countmap(values(K));
+            LL(x, grad) = -loglh_truncated(x[1], x[2], S, O, t, n, 1000);
             opt = Opt(:LN_SBPLX, 2);
             lower = [0.01, 0];
             opt.lower_bounds = lower;
             opt.min_objective = LL;
-            opt.xtol_abs = 0.5;
+            opt.xtol_abs = 0.1;
             (minf, minx, ret) = NLopt.optimize(opt, [5.0, 10.0]);
             push!(estis, [round(minx[2]) + length(O), alpha, t]);
+            push!(chaos, [round(chao(length(O), f)), alpha, t]);
+            push!(chaos_corr, [round(chao_corrected(length(O), t, f)), alpha, t]);
+            push!(jks, vcat([round(jackknife(length(O), t, f, k)) for k in 1:5], [alpha, t]));
             if t == 2
-                push!(links, [round(lincoln(S)), alpha, t]);
+                push!(links, [round(lincoln(S, n)), alpha, t]);
             end
             if t > 2
-                push!(schnab, [round(schnabel(S)), alpha, t]);
+                push!(schnab, [round(schnabel(n, t, K)), alpha, t]);
             end
         end
     end
