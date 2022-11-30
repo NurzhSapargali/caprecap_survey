7dcc6a245a3a9bc05bf6db45c0b6d4f23245acb9
Nurzhan Sapargali <nurzh.sapargali@gmail.com>
2022-11-30 20:07:34 +0100

    5 batches down

--- tweet_samples.py
+++ tweet_samples.py
@@ -11,47 +11,49 @@ client = tweepy.Client(consumer_key="",
                        access_token="",
                        access_token_secret="")
 dfs = []
-raw = pd.read_csv("./_200_input/tweets/corona_tweets_986.csv", header=None)
-indices = list(range(0, raw.shape[0], 100))
-for i in indices:
-    if i != indices[-1]:
-        cut = raw.iloc[i:(i + 100), 0].to_list()
-    else:
-        cut = raw.iloc[i:, 0].to_list()
-    timeout = True
-    while timeout:
-        try:
-            response = client.get_tweets(cut,
-                                         tweet_fields=["author_id", "in_reply_to_user_id", "referenced_tweets"],
-                                         expansions=["entities.mentions.username"])
-            timeout = False
-        except Exception as e:
-            print(e)
-            if e == tweepy.TooManyRequests:
-                sleep(15 * 64)
-            else:
-                sleep(30)
-    out = []
-    for tweet in response.data:
-        row = {}
-        row["tweet_id"] = str(tweet.id)
-        #row["text"] = str(tweet.text)
-        row["author_id"] = str(tweet.author_id)
-        if tweet.in_reply_to_user_id:
-            row["in_reply_to_user_id"] = str(tweet.in_reply_to_user_id)
-        if tweet.referenced_tweets:
-            for ref in tweet.referenced_tweets:
-                row[ref.type] = str(ref.id)
-        if tweet.entities:
-            ments = [m.get("id") for m in tweet.entities.get("mentions")]
-            row["mentions"] = ments
-        out.append(row)
-    dfs.append(pd.DataFrame(out))
-    print(len(dfs))
-overall = pd.concat(dfs)
-overall.to_csv("./_900_output/data/hydrated/hydrated_tweets_986.csv",
-               sep="\t",
-               index=False)
+for c in [0, 1]:
+    raw = pd.read_csv("./_200_input/tweets/corona_tweets_98{}.csv".format(c), header=None)
+    indices = list(range(0, raw.shape[0], 100))
+    for i in indices:
+        if i != indices[-1]:
+            cut = raw.iloc[i:(i + 100), 0].to_list()
+        else:
+            cut = raw.iloc[i:, 0].to_list()
+        timeout = True
+        while timeout:
+            try:
+                response = client.get_tweets(cut,
+                                             tweet_fields=["author_id", "in_reply_to_user_id", "referenced_tweets"],
+                                             expansions=["entities.mentions.username"],
+                                             user_auth=True)
+                timeout = False
+            except Exception as e:
+                print(e)
+                if e == tweepy.TooManyRequests:
+                    sleep(15 * 64)
+                else:
+                    sleep(30)
+        out = []
+        for tweet in response.data:
+            row = {}
+            row["tweet_id"] = str(tweet.id)
+            #row["text"] = str(tweet.text)
+            row["author_id"] = str(tweet.author_id)
+            if tweet.in_reply_to_user_id:
+                row["in_reply_to_user_id"] = str(tweet.in_reply_to_user_id)
+            if tweet.referenced_tweets:
+                for ref in tweet.referenced_tweets:
+                    row[ref.type] = str(ref.id)
+            if tweet.entities:
+                ments = [m.get("id") for m in tweet.entities.get("mentions")]
+                row["mentions"] = ments
+            out.append(row)
+        dfs.append(pd.DataFrame(out))
+        print(len(dfs))
+    overall = pd.concat(dfs)
+    overall.to_csv("./_900_output/data/hydrated/hydrated_tweets_98{}.csv".format(c),
+                   sep="\t",
+                   index=False)
     
 
 # NETWORK CONSTRUCTION
