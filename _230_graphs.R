487e579a1a59e202a747f8fce203f917824c7542
NurzhSapargali <nurzh.sapargali@gmail.com>
2024-01-29 06:57:21 +0600

    Graph simulation and estimation

--- _230_graphs.R
+++ _230_graphs.R
@@ -1,10 +1,60 @@
 library(igraph)
 
+# Simulation constants
 set.seed(123)
-NODES = c(5000, 10000)
+NODES = 10000
+EDGES = c(25000, 50000)
+SBM_BLOCK_SIZES = c(0.5 * NODES, 0.3 * NODES, 0.2 * NODES)
+# prob_mat = matrix(
+#   c(
+#     0.004345726,
+#     0.002172863,
+#     0.002172863,
+#     0.002172863,
+#     0.004345726,
+#     0.002172863,
+#     0.002172863,
+#     0.002172863,
+#     0.004345726
+#   ),
+#   byrow = TRUE,
+#   nrow = 3,
+#   ncol = 3
+# )
+# prob_mat = matrix(
+#   c(
+#     0.002897152,
+#     0.001448576,
+#     0.001448576,
+#     0.001448576,
+#     0.002897152,
+#     0.001448576,
+#     0.001448576,
+#     0.001448576,
+#     0.002897152
+#   ),
+#   byrow = TRUE,
+#   nrow = 3,
+#   ncol = 3
+# )
+SBM_BLOCK_MATRICES = list(
+  matrix(
+    c(0.0007, 0.00035, 0.00035, 0.00035, 0.0007, 0.00035, 0.00035, 0.00035, 0.0007),
+    byrow = TRUE,
+    nrow = 3,
+    ncol = 3
+  ),
+  matrix(
+    c(0.0014, 0.0007, 0.0007, 0.0007, 0.0014, 0.0007, 0.0007, 0.0007, 0.0014),
+    byrow = TRUE,
+    nrow = 3,
+    ncol = 3
+  )
+)
 DRAWS = 50
-TRIALS = 1
+TRIALS = 50
 BA_FOLDER = "./_200_input/graphs/ba_%s/"
+SBM_FOLDER = "./_200_input/graphs/sbm_%s/"
 
 # Write a sample graph to a file
 # Inputs:
@@ -12,9 +62,9 @@ BA_FOLDER = "./_200_input/graphs/ba_%s/"
 #  filename: The name of the file to write to
 # Outputs: None
 # Side Effects: Writes to a file
-write_sample <- function(s, filename) {
+write_sample = function(s, filename) {
   # Remove empty elements from the sample
-  clean <- Filter(length, s)
+  clean = Filter(length, s)
   # Only write to the file if there are elements in the cleaned sample
   if (length(clean) > 0) {
     # Write the cleaned sample to the file
@@ -33,76 +83,91 @@ write_sample <- function(s, filename) {
 # filenames: The filenames to write the samples to
 # Outputs: None
 # Side Effects: Writes to a file
-produce_samples <- function(graph, roots, order, filenames) {
+produce_samples = function(graph, roots, order, filenames) {
   # Create an ego graph from the input graph
-  ego_graph <- make_ego_graph(graph, order = order, nodes = roots)
-  # Extract the vertex names from the ego graph
-  vertex_names <- lapply(ego_graph, function(x) { V(x)$name })
-  # Write the vertex names to the specified file
+  ego_graph = make_ego_graph(graph, order = order, nodes = roots)
+  # Extract vertex names from the ego graph
+  vertex_names = lapply(ego_graph, function(x) { V(x)$name })
+  # Write vertex names to the specified file
   write_sample(vertex_names, filenames[1])
 }
 
-sbm_block_sizes = lapply(NODES, function(x) {c(x * 0.5, x * 0.35, x * 0.15)})
-pa_edges_per_node = lapply(
-  NODES,
-  function(x) {
-    round(c(
-      (x - 1) / 2 * 0.20,
-      (x - 1) / 2 * 0.40
-    ))
-  }
-)
-names(sbm_block_sizes) = NODES
-names(pa_edges_per_node) = NODES
-sbm_matrices = list(
-  matrix(
-    c(0.15, 0.05, 0.05, 0.05, 0.15, 0.05, 0.05, 0.05, 0.15),
-    nrow = 3,
-    ncol = 3,
-    byrow = TRUE
-  ),
-  matrix(
-    c(0.6, 0.08, 0.08, 0.08, 0.6, 0.08, 0.08, 0.08, 0.6),
-    nrow = 3,
-    ncol = 3,
-    byrow = TRUE
+# Simulate preferential attachment graphs
+for (e in EDGES) {
+  write(
+    c("N", "edges"),
+    file = sprintf(
+      paste(BA_FOLDER, "metadata_%s.csv", sep = ""),
+      e / 1000,
+      e / 1000
+    ),
+    ncolumns = 2,
+    append = TRUE,
+    sep = ","
   )
-)
-for (n in NODES) {
-  for (e in PA_EDGES_PER_NODE) {
-    G =
-      sample_pa(n, 1, m = e, directed = FALSE) %>%
-      set.vertex.attribute("name", value = seq(1, n))
-    c(
-      "N",
-      "edges",
-      "triangles",
-      "4_cliques"
-      ) %>%
+  for (t in seq(1, TRIALS)) {
+    gpa =
+      sample_pa(NODES, 1, m = e / NODES, directed = FALSE) %>%
+      set.vertex.attribute("name", value = seq(1, NODES))
+    # Write out number of edges and nodes of simulated graph
     write(
-      file = sprintf(paste(BA_FOLDER, "metadata_%s.csv", sep = ""), e, n),
-      ncolumns = 4,
+      c(vcount(gpa), ecount(gpa)),
+      file = sprintf(
+        paste(BA_FOLDER, "metadata_%s.csv", sep = ""),
+        e / 1000,
+        e / 1000
+      ),
+      ncolumns = 2,
       append = TRUE,
       sep = ","
-      )
-    c(
-      vcount(G),
-      ecount(G),
-      sum(count_triangles(G)) / 3,
-      length(cliques(G, min = 4, max = 4))
+    )
+    # Write out samples of nodes from snowball sampling to a separate file
+    roots = sample(1:NODES, DRAWS)
+    filenames = c(
+      sprintf(paste(BA_FOLDER, "graph_%s.csv", sep = ""), e / 1000, t)
+    )
+    produce_samples(gpa, roots, 2, filenames)
+  }
+}
+# Simulate stochastic block model graphs
+for (m in seq_along(SBM_BLOCK_MATRICES)) {
+  write(
+    c("N", "edges"),
+    file = sprintf(
+      paste(SBM_FOLDER, "metadata_%s.csv", sep = ""),
+      EDGES[m] / 1000,
+      EDGES[m] / 1000
+    ),
+    ncolumns = 2,
+    append = TRUE,
+    sep = ","
+  )
+  for (t in seq(1, TRIALS)) {
+    gsbm =
+      sample_sbm(
+        NODES,
+        SBM_BLOCK_MATRICES[[m]],
+        SBM_BLOCK_SIZES,
+        directed = FALSE
       ) %>%
+      set.vertex.attribute("name", value = seq(1, NODES))
+    # Write out number of edges and nodes of simulated graph
     write(
-      file = sprintf(paste(BA_FOLDER, "metadata_%s.csv", sep = ""), e, n),
-      ncolumns = 4,
+      c(vcount(gsbm), ecount(gsbm)),
+      file = sprintf(
+        paste(SBM_FOLDER, "metadata_%s.csv", sep = ""),
+        EDGES[m] / 1000,
+        EDGES[m] / 1000
+      ),
+      ncolumns = 2,
       append = TRUE,
       sep = ","
-      )
-    for (t in seq(1, TRIALS)) {
-      roots = sample(1:n, DRAWS)
-      filenames = c(
-        sprintf(paste(BA_FOLDER, "nodes_%s_%s.csv", sep = ""), e, t, n)
-        )
-      produce_samples(G, roots, 2, filenames)
-    }
+    )
+    # Write out samples of nodes from snowball sampling to a separate file
+    roots = sample(1:NODES, DRAWS)
+    filenames = c(
+      sprintf(paste(SBM_FOLDER, "graph_%s.csv", sep = ""), EDGES[m] / 1000, t)
+    )
+    produce_samples(gsbm, roots, 2, filenames)
   }
 }
--- _240_estimate_graphs.jl
+++ _240_estimate_graphs.jl
@@ -1,75 +1,85 @@
-include("_110_estimator.jl")
 include("_100_utils.jl")
-include("_120_benchmarks.jl")
+include("_120_gamma_estimator.jl")
+include("_130_benchmarks.jl")
 
-import .Estimator
+import .GammaEstimator
 import .Utils
 import .Benchmarks
 
 using StatsBase
-using DelimitedFiles
 
-BA_EDGES_PER_NODE::Vector{Int64} = [1, 2, 3]
-TRIALS::Int64 = 500
-breaks_T::Vector{Int64} = [5, 10, 15, 20]
+import Random: seed!
+
+GRAPHS::Vector{String} = ["ba_25", "ba_50", "sbm_25", "sbm_50"]
 DATA_FOLDER::String = "./_200_input/graphs/"
+breaks_T::Vector{Int64} = collect(5:5:50)
 OUTPUT_FOLDER::String = "./_900_output/data/graphs/"
-pops::Vector{Int64} = [1000, 5000, 10000]
+SEED::Int = 777
+TRUE_N::Int = 10000
 
-function get_truth(filename)
-    data = readdlm(filename, ' ', String, '\n')[2, 1]
-    return parse(Float64, split(data, ",")[1])
-end
-
-
-for epn in BA_EDGES_PER_NODE
-    output_file = OUTPUT_FOLDER * "nodes_estimates_$(epn).csv"
-    Utils.write_row(output_file, ["a_hat", "Nu_hat", "N_hat", "No", "trial", "T", "avg_n", "N", "type"])
-    for N in pops
-        metafile = DATA_FOLDER * "ba_$(epn)/metadata_$N.csv"
-        # N = get_truth(metafile)
-        data_folder = DATA_FOLDER * "ba_$(epn)/"
-        data_files = [file for file in readdir(data_folder) if occursin("nodes", file) && occursin("$(N).csv", file)]
-        for file in data_files
-            samples = Utils.read_captures(data_folder * file)
-            trial_no = parse(Int, split(split(file, "_")[2], ".")[1])
-            draws = []
-            for t in breaks_T
-                S = samples[1:t]
-                K = Dict{Int, Int}()
-                for s in S
-                    addcounts!(K, s)
-                end
-                f = countmap(values(K))
-                println("***TRIAL NO $file, $t***")
-                O = Set([i for j in S for i in j])
-                n = [length(s) for s in S]
-                No = length(O)
-                sum_x = collect(values(K))
-                theta = [log(No), log(5.0)]
-                theta = Estimator.fit_model(theta, n, No, sum_x)
-                N_hat = exp(theta[1]) + No
-                println([exp(theta[2]), exp(theta[1]), N_hat, length(O), trial_no, t, mean(n), N])
-                push!(draws, [exp(theta[2]), exp(theta[1]), N_hat, length(O), trial_no, t, mean(n), N, "Pseudolikelihood"])
-                benchmarks = Dict{}()
-                benchmarks["Schnabel"] = Benchmarks.schnabel(S, n)
-                benchmarks["Chao"] = Benchmarks.chao(length(O), f)
-                benchmarks["Zelterman"] = Benchmarks.zelterman(length(O), f)
-                benchmarks["Conway-Maxwell-Poisson"] = Benchmarks.conway_maxwell(length(O), f)
-                benchmarks["Huggins"] = Benchmarks.huggins(t, K)
-                benchmarks["Turing Geometric"] = Benchmarks.turing_geometric(length(O), f, t)
-                benchmarks["Turing"] = Benchmarks.turing(length(O), f, t)
-                for k in 1:5
-                    jk = Benchmarks.jackknife(length(O), t, f, k)
-                    benchmarks["Jackknife k = $(k)"] = jk
-                end
-                for b in keys(benchmarks)
-                    push!(draws, [-999.0, benchmarks[b] - No, benchmarks[b], length(O), trial_no, t, mean(n), N, b])
-                end
+seed!(SEED)
+for i in 1:length(GRAPHS)
+    graph = GRAPHS[i]
+    output_file = OUTPUT_FOLDER * "estimates_$(graph).csv"
+    Utils.write_row(output_file, ["a_hat", "Nu_hat", "N_hat", "No", "trial", "T", "graph", "N", "type"])
+    data_folder = DATA_FOLDER * "$(graph)/"
+    data_files = [file for file in readdir(data_folder) if occursin("graph", file)]
+    for file in data_files
+        trial_no = parse(Int, split(split(file, "_")[2], ".")[1])
+        samples = Utils.read_captures(data_folder * file)
+        draws = fill([], length(breaks_T) * 15)
+        Threads.@threads for j in 1:length(breaks_T)
+            t = breaks_T[j]
+            S = samples[1:t]
+            K = Utils.cap_freq(S)
+            f = Utils.freq_of_freq(K)
+            println("***TRIAL NO $file, $t***")
+            O = Set([i for j in S for i in j])
+            n = [length(s) for s in S]
+            No = length(O)
+            benchmarks = Dict{}()
+            benchmarks["Turing"] = Benchmarks.turing(No, f, t)
+            (minf, minx, ret) = GammaEstimator.fit_Gamma(
+                [5.0, No],
+                n,
+                No,
+                K
+            )
+            N_hat = No + minx[2]
+            println([minx[1], minx[2], N_hat, No, trial_no, t, graph, 10000])
+            counter = 1
+            draws[(j - 1) * 15 + counter] = [minx[1], minx[2], N_hat, No, trial_no, t, graph, TRUE_N, "Pseudolikelihood"]
+            #push!(draws, [minx[1], minx[2], N_hat, No, trial_no, t, graph, TRUE_N, "Pseudolikelihood"])
+            benchmarks["Schnabel"] = Benchmarks.schnabel(S, n)
+            benchmarks["Chao"] = Benchmarks.chao(No, f)
+            benchmarks["Zelterman"] = Benchmarks.zelterman(No, f)
+            benchmarks["Conway-Maxwell-Poisson"] = Benchmarks.conway_maxwell(No, f)
+            benchmarks["Turing Geometric"] = Benchmarks.turing_geometric(No, f, t)
+            #benchmarks["Morgan Ridout"] = Benchmarks.morgan_ridout(f, t, "./estimateN.R")
+            for b in 0:2
+                benchmarks["Chao Lee Jeng $b"] = Benchmarks.chao_lee_jeng(No, f, t, n, b)
             end
-            for d in draws
-                Utils.write_row(output_file, d)
+            for k in 1:5
+                jk = Benchmarks.jackknife(No, t, f, k)
+                benchmarks["Jackknife k = $(k)"] = jk
             end
+            for b in keys(benchmarks)
+                counter += 1
+                draws[(j - 1) * 15 + counter] = [-999.0, benchmarks[b] - No, benchmarks[b], No, trial_no, t, graph, TRUE_N, b]
+                #push!(draws, [-999.0, benchmarks[b] - No, benchmarks[b], No, trial_no, t, graph, TRUE_N, b])
+            end
+        end
+        for t in breaks_T
+            S = samples[1:t]
+            K = Utils.cap_freq(S)
+            f = Utils.freq_of_freq(K)
+            mr_estimator = Benchmarks.morgan_ridout(f, t, "./estimateN.R")
+            O = Set([i for j in S for i in j])
+            No = length(O)
+            push!(draws, [-999.0, mr_estimator - No, mr_estimator, No, trial_no, t, graph, TRUE_N, "Morgan Ridout"])
+        end
+        for d in draws
+            Utils.write_row(output_file, d)
         end
     end
 end
