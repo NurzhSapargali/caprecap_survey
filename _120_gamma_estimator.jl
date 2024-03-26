45680781bbb089605af39c2180e50d12e7ba81a6
Nurzhan Sapargali <nurzh.sapargali@gmail.com>
2024-03-26 19:27:06 +0100

    Remove unnecessary gradients and likelihood function

--- _120_gamma_estimator.jl
+++ _120_gamma_estimator.jl
@@ -4,25 +4,7 @@ using NLopt
 
 export loglh_redux, gradient_a_redux, gradient_Nu_redux
 
-function loglh(Nu, alpha, n, No, x_sums; verbose::Bool = true)
-    N = Nu + No
-    fact_term = 0.0
-    for i in keys(x_sums)
-        incs = get(x_sums, i, 0)
-        range_inc = sum([log(alpha + incs - j) for j in 1:incs])
-        fact_term += range_inc
-    end
-    large_term = alpha * log(alpha * N + sum(n)) + log( 1.0 - ( alpha * N / (alpha * N + sum(n)) )^alpha )
-    lh = ( No * alpha * (log(alpha) + log(N))
-          - log(alpha * N + sum(n)) * sum(n)
-          + fact_term
-          - No * large_term )
-    if verbose
-        println("a = $alpha, b = $(N * alpha), N = $N, lh = $lh")
-    end
-    return lh
-end
-    
+
 function loglh_redux(log_alpha, log_Nu, n, No, ff; verbose::Bool = true)
     alpha = exp(log_alpha)
     Nu = exp(log_Nu)
@@ -42,24 +24,6 @@ function loglh_redux(log_alpha, log_Nu, n, No, ff; verbose::Bool = true)
         println("a = $alpha, b = $(N * alpha), N = $N, lh = $lh")
     end
     return lh
-end
-
-function gradient_a(log_Nu, log_a, n, No, sum_x)
-    Nu = exp(log_Nu)
-    N = Nu + No
-    alpha = exp(log_a)
-    beta = N * alpha
-    beta_tilde = beta + sum(n)
-    inner_del_num = ( beta_tilde^alpha * log(beta_tilde)
-                    + beta * beta_tilde^(alpha - 1.0)
-                    - beta^alpha * log(beta) )
-    inner_del_denom = beta_tilde^alpha - beta^alpha
-    inner_del = inner_del_num / inner_del_denom
-    inner_del += digamma(alpha) - log(alpha) - 1.0
-    sum_term = sum(digamma.(alpha .+ sum_x))
-    out = (-No * inner_del - N / beta_tilde * sum(sum_x) + sum_term) * alpha
-    println(out)
-    return out
 end
 
 function gradient_a_redux(log_alpha, log_Nu, n, No, ff)
@@ -89,23 +53,8 @@ function gradient_Nu_redux(log_alpha, log_Nu, n, No)
     N = Nu + No
     ratio = 1.0 + sum(n) / exp(log_alpha + log(N))
     inner_derivative = (ratio^(alpha - 1.0)  - 1.0) / (ratio^alpha - 1.0) * alpha / N
-    println(inner_derivative)
     out = ( alpha * No / N - No * inner_derivative - sum(n) / N * 1.0 / ratio )
     return out * Nu
-end
-
-function gradient_Nu(log_Nu, log_a, n, No, sum_x)
-    Nu = exp(log_Nu)
-    N = Nu + No
-    alpha = exp(log_a)
-    beta = N * alpha
-    beta_tilde = beta + sum(n)
-    inner_del_num = alpha^2 * beta_tilde^(alpha - 1.0) - beta^(alpha - 1.0) * alpha^2
-    inner_del_denom = beta_tilde^alpha - beta^alpha
-    inner_del = inner_del_num / inner_del_denom
-    inner_del += -alpha / N
-    out = (-No * inner_del - alpha / beta_tilde * sum(sum_x)) * Nu
-    return out
 end
 
 function fit_Gamma(theta0, n, No, ff; lower::Vector = [-Inf, -Inf], upper::Vector = [10, 12], ftol = 1e-7)
@@ -130,6 +79,55 @@ function fit_Gamma(theta0, n, No, ff; lower::Vector = [-Inf, -Inf], upper::Vecto
     (minf,minx,ret) = optimize(opt, theta0)
     return (minf, minx, ret)
 end
+
+#= function loglh_oi(log_alpha, log_Nu, log_w, n, ff, nn; verbose::Bool = true)
+    w = exp(log_w)
+    alpha = exp(log_alpha)
+    Nu = exp(log_Nu)
+    No = sum(values(ff))
+    No_1 = No - sum([length(nn[i]) for i in keys(nn)])
+    N = Nu + No
+    b = exp(log_Nu + log(No) + log_alpha)
+    log_bn = log(b + sum(n))
+    coef_term = 0.0
+    fact_term = 0.0
+    for k in 2:maximum(keys(ff))
+        multiplier = get(ff, k, 0)
+        range_inc = sum([log(alpha + k - j) for j in 1:k])
+        fact_term += multiplier * range_inc
+        coef_term += multiplier * (alpha + k)
+    end
+    coef_term *= log_bn
+    logratio_term = alpha * log(b) - (alpha + 1) * log_bn + log(alpha) + log(1 - w)
+    last_term = 0.0
+    for l in keys(nn)
+        multiplier = length(get(nn, l, 0))
+        last_term += multiplier * log(w + exp(logratio_term) * l)
+    end
+    truncation = ( log(w - 1 + (1 + sum(n) / b)^alpha)
+                  - alpha * log(1 + sum(n) / b) )
+    truncation *= -No
+    non_one = No_1 * (log(1 - w) + alpha * log(b))
+    lh = (truncation + non_one - coef_term + fact_term + last_term)
+    if verbose
+        println("w = $w, a = $alpha, b = $(N * alpha), N = $N, lh = $lh")
+    end
+    return lh
+end =#
+#= 
+function fit_Gamma_oi(theta0, n, ff, nn; lower::Vector = [-Inf, -Inf, -Inf], upper::Vector = [10, 12, 0.0], ftol = 1e-7)
+    LL(x, grad) = -loglh_oi(x[1], x[2], x[3], n, ff, nn)
+    #opt = Opt(:LD_LBFGS, 2)
+    opt = Opt(:LN_SBPLX, 3)
+    opt.min_objective = LL
+    opt.ftol_abs = ftol
+    opt.lower_bounds = lower
+    opt.upper_bounds = upper
+    opt.maxeval = 10000
+    #opt.vector_storage = 10
+    (minf,minx,ret) = optimize(opt, theta0)
+    return (minf, minx, ret)
+end =#
 
 
 end
