483e7de28847291ccfbf6e2a287867e142a8f6ab
NurzhSapargali <nurzh.sapargali@gmail.com>
2024-04-05 14:11:19 +0500

    Appendix plotting script almost done

--- _610_chao_poisson_comparison.R
+++ _610_chao_poisson_comparison.R
@@ -1,6 +1,5 @@
-library(ggplot2)
-library(magrittr)
-library(dplyr)
+library(tidyverse)
+library(ggpubr)
 
 TRIALS <- 1000
 SEED <- 777
@@ -22,40 +21,90 @@ pseudo <- function(f) {
   return(chao(f) - f[2])
 }
 
-turing_b <- function(f) {
-  return(f[1] + f[2] + f[1]^2 / (4 * f[2]))
-}
-
 turing_p <- function(f) {
   return(3 / 2 * f[1] + f[2] + f[1]^2 / (2 * f[2]))
 }
 
-set.seed(SEED)
-res <- list()
-for (n in SAMPLE_SIZES) {
-  reps <- t(replicate(TRIALS, srswor_caprecap(n, n, POP_SIZE)))
-  reps <- cbind(
-    reps,
-    apply(reps, 1, chao),
-    apply(reps, 1, pseudo),
-    apply(reps, 1, turing_p),
-    apply(reps, 1, turing_b),
-    n
-  )
-  res[[as.character(n)]] <- reps
+simulate_sampling <- function(n1, n2 = NA, N) {
+  res <- list()
+  if (!anyNA(n2)) {
+    for (i in n1) {
+      for (j in n2) {
+        reps <- t(replicate(TRIALS, srswor_caprecap(i, j, N)))
+        reps <- cbind(
+          reps,
+          apply(reps, 1, chao),
+          apply(reps, 1, pseudo),
+          apply(reps, 1, turing_p),
+          i,
+          j
+        )
+        res[[paste0(i, j)]] <- reps
+      }
+    }
+  } else {
+    for (i in n1) {
+      reps <- t(replicate(TRIALS, srswor_caprecap(i, i, N)))
+      reps <- cbind(
+        reps,
+        apply(reps, 1, chao),
+        apply(reps, 1, pseudo),
+        apply(reps, 1, turing_p),
+        i,
+        i
+      )
+      res[[paste0(i, i)]] <- reps
+    }
+  }
+  res <- do.call(rbind, res) %>%
+    as.data.frame() %>%
+    setNames(
+      c(
+        "f1",
+        "f2",
+        "Chao",
+        "Pseudolikelihood",
+        "Turing Poisson",
+        "n_1",
+        "n_2"
+      )
+    )
+  agg <- res %>%
+    group_by(n_1, n_2) %>%
+    summarise(
+      `Chao` = mean(`Chao`),
+      `Pseudolikelihood` = mean(`Pseudolikelihood`),
+      `Turing Poisson` = mean(`Turing Poisson`),
+      .groups = "keep"
+    )
+  return(agg)
 }
-res <- do.call(rbind, res) %>%
-  as.data.frame() %>%
-  setNames(c("f1", "f2", "chao", "pseudo", "turing_p", "turing_b", "n"))
-agg <- res %>%
-  group_by(n) %>%
-  summarise(
-    chao = mean(chao),
-    pseudo = mean(pseudo),
-    turing_p = mean(turing_p),
-    turing_b = mean(turing_b)
-  )
 
-ggplot(data = res, mapping = aes(group = 2 * n / POP_SIZE, y = turing_b)) +
-  geom_boxplot() +
-  geom_hline(yintercept = POP_SIZE, linetype = "dashed")
+set.seed(SEED)
+first_fixed <- simulate_sampling(c(100), SAMPLE_SIZES, POP_SIZE)
+first_fixed$type <- "n1 = 100"
+second_fixed <- simulate_sampling(SAMPLE_SIZES, c(100), POP_SIZE)
+second_fixed$type <- "n2 = 100"
+none_fixed <- simulate_sampling(SAMPLE_SIZES, NA, POP_SIZE)
+none_fixed$type <- "n1 = n2"
+
+agg <- rbind(first_fixed, none_fixed, second_fixed) %>%
+  pivot_longer(
+    cols = c(`Chao`, `Pseudolikelihood`, `Turing Poisson`),
+    names_to = "method",
+    values_to = "estimate"
+  )
+agg$type <- factor(agg$type, levels = c("n1 = 100", "n2 = 100", "n1 = n2"))
+
+ggplot(
+  agg,
+  mapping = aes(x = (n_1 + n_2) / POP_SIZE, y = estimate, colour = method)
+) +
+  geom_line() +
+  geom_point() +
+  geom_hline(yintercept = POP_SIZE, color = "grey", alpha = 0.7) +
+  xlab(expression(("n"[1] + "n"[2]) / N)) +
+  theme_pubr() +
+  facet_wrap(vars(type), scales = "free_y")
+
+ggsave("check.pdf")
