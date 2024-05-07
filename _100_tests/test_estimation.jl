0e371413133a1587c56d9efad2f568b3de4cd87e
Nurzhan Sapargali <nurzh.sapargali@gmail.com>
2024-05-07 16:18:35 +0200

    No sample sizes

--- _100_tests/test_estimation.jl
+++ _100_tests/test_estimation.jl
@@ -16,11 +16,10 @@ using StatsFuns
 import Random: seed!
 
 
-N = 1000000
-a = 0.05
-T = 50
-n = repeat([37, 44, 100, 17, 2, 75, 17, 2, 112, 3] * 1000, 100)[1:T]
-n = repeat([2] * 100000, T)
+N = 10000
+a = 0.5
+T = 200
+n = repeat([37, 44, 100, 17, 2, 75, 17, 2, 112, 3], 100)[1:T]
 b = a * (N / maximum(n) - 1.0)
 trials = 1
 d = Beta(a, b)
@@ -36,33 +35,34 @@ for s in S
     addcounts!(K, s)
 end
 f = Dict(5 => 6, 4 => 21, 6 => 3, 2 => 243, 3 => 58, 1 => 1253) # At T = 50 causes error
+f = Dict(1 => 48, 2 => 9, 3 => 6, 4 => 2, 5 => 0, 6 => 2, 7 => 0, 8 => 2, 9 => 1, 10 => 1, 12 => 1, 13 => 1, 16 => 1, 17 => 2, 18 => 1, 20 => 1, 29 => 1, 42 => 1, 53 => 1)
 f = countmap(values(K))
 O = Set([i for j in S for i in j])
 converged = false
-theta = [log(5.0), log(2.0)]
+theta = [log(1.0), log(1.0)]
 (minf1, minx1, ret1) = GammaEstimator.fit_Gamma(
     theta,
-    n,
     f,
     ftol = 1e-7,
-    upper = [10, 20]
+    upper = [10, 20],
+    lower = [-30, -30],
 )
 N_hat1 = length(K) + exp(minx1[2])
 # N_hat2 = minx2[2] + length(X)
 # push!(row, N_hat2)
 benchmarks = Dict{}()
 benchmarks["Schnabel"] = Benchmarks.schnabel(S, n)
-benchmarks["Chao"] = Benchmarks.chao(length(O), f)
-benchmarks["Zelterman"] = Benchmarks.zelterman(length(O), f)
-benchmarks["Conway-Maxwell-Poisson"] = Benchmarks.conway_maxwell(length(O), f)
-benchmarks["Turing Geometric"] = Benchmarks.turing_geometric(length(O), f, T)
-benchmarks["Turing"] = Benchmarks.turing(length(O), f, T)
+benchmarks["Chao"] = Benchmarks.chao(sum(values(f)), f)
+benchmarks["Zelterman"] = Benchmarks.zelterman(sum(values(f)), f)
+benchmarks["Conway-Maxwell-Poisson"] = Benchmarks.conway_maxwell(sum(values(f)), f)
+benchmarks["Turing Geometric"] = Benchmarks.turing_geometric(sum(values(f)), f, T)
+benchmarks["Turing"] = Benchmarks.turing(sum(values(f)), f, T)
 benchmarks["Morgan Ridout"] = Benchmarks.morgan_ridout(f, T, "./estimateN.R")
 for b in 0:2
     benchmarks["Chao Lee Jeng $b"] = Benchmarks.chao_lee_jeng(length(O), f, T, n, b)
 end
 for k in 1:5
-    jk = Benchmarks.jackknife(length(O), T, f, k)
+    jk = Benchmarks.jackknife(sum(values(f)), maximum(keys(f)), f, k)
     benchmarks["Jackknife k = $(k)"] = jk
 end
 println(benchmarks)
--- _120_gamma_estimator.jl
+++ _120_gamma_estimator.jl
@@ -9,13 +9,14 @@ export hessian, hessian_log
 export fit_Gamma
 
 
-function loglh(log_alpha, log_Nu, n, ff; verbose::Bool = true)
+function loglh(log_alpha, log_Nu, ff; verbose::Bool = true)
     alpha = exp(log_alpha)
     Nu = exp(log_Nu)
     No = sum(values(ff))
     N = Nu + No
     fact_term = 0.0
-    ratio = 1.0 + sum(n) / alpha / N
+    sum_n = sum([get(ff, i, 0) * i for i in keys(ff)])
+    ratio = 1.0 + sum_n / alpha / N
     # upper_N = 2 * No - get(ff, maximum(keys(ff)), 0)
     for i in keys(ff)
         multiplier = get(ff, i, 0)
@@ -23,7 +24,7 @@ function loglh(log_alpha, log_Nu, n, ff; verbose::Bool = true)
         fact_term += multiplier * range_inc
     end
     lh = ( -No * log(ratio^alpha - 1.0)
-           - sum(n) * (log(alpha) + log(N) + log(ratio))
+           - sum_n * (log(alpha) + log(N) + log(ratio))
            + fact_term )
     if verbose
         println("a = $alpha, b = $(N * alpha), N = $N, lh = $lh")
@@ -32,11 +33,12 @@ function loglh(log_alpha, log_Nu, n, ff; verbose::Bool = true)
 end
 
 
-function gradient_a(alpha, Nu, n, ff)
+function gradient_a(alpha, Nu, ff)
     No = sum(values(ff))
     N = Nu + No
-    ratio = 1.0 + sum(n) / alpha / N
-    ratio_derivative = -sum(n) / alpha^2 / N
+    sum_n = sum([get(ff, i, 0) * i for i in keys(ff)])
+    ratio = 1.0 + sum_n / alpha / N
+    ratio_derivative = -sum_n / alpha^2 / N
     fact_term = 0.0
     for i in keys(ff)
         multiplier = get(ff, i, 0)
@@ -45,43 +47,45 @@ function gradient_a(alpha, Nu, n, ff)
     end
     gradient = ( -No / (ratio^alpha - 1.0)
                  * (ratio^alpha * (log(ratio) + alpha / ratio * ratio_derivative))
-                 - sum(n) * (1.0 / alpha + 1.0 / ratio * ratio_derivative)
+                 - sum_n * (1.0 / alpha + 1.0 / ratio * ratio_derivative)
                  + fact_term )
     #println("....a = $alpha, b = $(N * alpha), N = $N, grad_a = $gradient")
     return gradient
 end
 
 
-function gradient_log_a(log_alpha, log_Nu, n, ff)
+function gradient_log_a(log_alpha, log_Nu, ff)
     alpha = exp(log_alpha)
     Nu = exp(log_Nu)
-    return gradient_a(alpha, Nu, n, ff) * alpha
+    return gradient_a(alpha, Nu, ff) * alpha
 end
 
 
-function gradient_Nu(alpha, Nu, n, ff)
+function gradient_Nu(alpha, Nu, ff)
     No = sum(values(ff))
     N = Nu + No
-    ratio = 1.0 + sum(n) / alpha / N
-    ratio_derivative = -sum(n) / alpha / N^2
+    sum_n = sum([get(ff, i, 0) * i for i in keys(ff)])
+    ratio = 1.0 + sum_n / alpha / N
+    ratio_derivative = -sum_n / alpha / N^2
     gradient = ( -No / (ratio^alpha - 1.0) * alpha * ratio^(alpha - 1.0) * ratio_derivative
-                 - sum(n) * (1.0 / N + ratio_derivative / ratio) )
+                 - sum_n * (1.0 / N + ratio_derivative / ratio) )
     #println("....a = $alpha, b = $(N * alpha), N = $N, grad_Nu = $gradient")
     return gradient
 end
 
 
-function gradient_log_Nu(log_alpha, log_Nu, n, ff)
+function gradient_log_Nu(log_alpha, log_Nu, ff)
     alpha = exp(log_alpha)
     Nu = exp(log_Nu)
-    return gradient_Nu(alpha, Nu, n, ff) * Nu
+    return gradient_Nu(alpha, Nu, ff) * Nu
 end
 
 
-function inner_del2_a(alpha, Nu, n, ff)
+function inner_del2_a(alpha, Nu, ff)
     N = Nu + sum(values(ff))
-    g_a = 1.0 + sum(n) / alpha / N
-    del_g_a = -sum(n) / N / alpha^2
+    sum_n = sum([get(ff, i, 0) * i for i in keys(ff)])
+    g_a = 1.0 + sum_n / alpha / N
+    del_g_a = -sum_n / N / alpha^2
     del_g_a_a_log = (g_a^alpha * (log(g_a) + alpha / g_a * del_g_a) * log(g_a)
                      + g_a^(alpha - 1) * del_g_a)
     del_g_a_a = g_a^alpha * (log(g_a) + alpha / g_a * del_g_a)
@@ -90,49 +94,53 @@ function inner_del2_a(alpha, Nu, n, ff)
     return (1.0 / alpha
             + (del_g_a_a_log * (g_a^alpha - 1.0) - g_a^alpha * log(g_a) * del_g_a_a)
             / (g_a^alpha - 1.0)^2
-            - sum(n) / N
+            - sum_n / N
             * (del_g_a_a_1 * alpha * (g_a^alpha - 1.0) - g_a^(alpha - 1.0) * del_g_a_a_a_1)
             / (alpha * (g_a^alpha - 1.0))^2 )
 end
 
 
-function del2_a(alpha, Nu, n, ff)
+function del2_a(alpha, Nu, ff)
     No = sum(values(ff))
     N = Nu + No
-    g_a = 1.0 + sum(n) / alpha / N
+    sum_n = sum([get(ff, i, 0) * i for i in keys(ff)])
+    g_a = 1.0 + sum_n / alpha / N
     fact_term = 0.0
     for i in keys(ff)
         multiplier = get(ff, i, 0)
         range_inc = sum([1.0 / (alpha + i - j)^2 for j in 1:i])
         fact_term += multiplier * range_inc
     end 
-    return (No * (1.0 / alpha - inner_del2_a(alpha, Nu, n, ff))
+    return (No * (1.0 / alpha - inner_del2_a(alpha, Nu, ff))
             - fact_term
-            + sum(n) / alpha / alpha / g_a^2)
+            + sum_n / alpha / alpha / g_a^2)
 end
 
 
-function inner_del2_N(alpha, Nu, n, ff)
+function inner_del2_N(alpha, Nu, ff)
+    sum_n = sum([get(ff, i, 0) * i for i in keys(ff)])
     N = Nu + sum(values(ff))
-    g_a = 1.0 + sum(n) / alpha / N
+    g_a = 1.0 + sum_n / alpha / N
     return ( alpha / N * (alpha - 1.0) / N * (g_a^(alpha - 2.0) - 1.0) / (g_a^alpha - 1.0)
             - (alpha / N * (g_a^(alpha - 1.0) - 1.0) / (g_a^alpha - 1.0))^2 )
 end
 
 
-function del2_Nu(alpha, Nu, n, ff)
+function del2_Nu(alpha, Nu, ff)
+    sum_n = sum([get(ff, i, 0) * i for i in keys(ff)])
     No = sum(values(ff))
     N = Nu + No
-    g_a = 1.0 + sum(n) / alpha / N
-    return (No * (-alpha / N / N - inner_del2_N(alpha, Nu, n, ff))
-            + sum(n) / N / N / g_a^2)
+    g_a = 1.0 + sum_n / alpha / N
+    return (No * (-alpha / N / N - inner_del2_N(alpha, Nu, ff))
+            + sum_n / N / N / g_a^2)
 end
 
 
-function inner_del_a_Nu(alpha, Nu, n, ff)
+function inner_del_a_Nu(alpha, Nu, ff)
+    sum_n = sum([get(ff, i, 0) * i for i in keys(ff)])
     N = Nu + sum(values(ff))
-    g_a = 1.0 + sum(n) / alpha / N
-    del_g_a = -sum(n) / N / alpha^2
+    g_a = 1.0 + sum_n / alpha / N
+    del_g_a = -sum_n / N / alpha^2
     del_g_a_a_1 = g_a^(alpha - 1.0) * (log(g_a) + (alpha - 1.0) / g_a * del_g_a)
     del_g_a_a = g_a^alpha * (log(g_a) + alpha / g_a * del_g_a)
     return (1.0 / N * (g_a^(alpha - 1.0) - 1.0) / (g_a^alpha - 1.0)
@@ -142,35 +150,33 @@ function inner_del_a_Nu(alpha, Nu, n, ff)
 end
 
 
-function cross_del_a_Nu(alpha, Nu, n, ff)
+function cross_del_a_Nu(alpha, Nu, ff)
+    sum_n = sum([get(ff, i, 0) * i for i in keys(ff)])
     N = Nu + sum(values(ff))
     No = sum(values(ff))
-    g_a = 1.0 + sum(n) / alpha / N
-    del_g_a = -sum(n) / N / alpha^2
-    return (No * (1.0 / N - inner_del_a_Nu(alpha, Nu, n, ff))
-            + sum(n) / N / g_a^2 * del_g_a)
+    g_a = 1.0 + sum_n / alpha / N
+    del_g_a = -sum_n / N / alpha^2
+    return (No * (1.0 / N - inner_del_a_Nu(alpha, Nu, ff))
+            + sum_n / N / g_a^2 * del_g_a)
 end
 
 
-function hessian(alpha, Nu, n, ff; lamb::Real = 0.0)
+function hessian(alpha, Nu, ff)
     hess = zeros(2, 2)
-    hess[1, 1] = del2_a(alpha, Nu, n, ff)
-    if lamb > 0
-        hess[1, 1] += del2_prior(alpha, lamb)
-    end
-    hess[2, 2] = del2_Nu(alpha, Nu, n, ff)
-    hess[1, 2] = cross_del_a_Nu(alpha, Nu, n, ff)
+    hess[1, 1] = del2_a(alpha, Nu, ff)
+    hess[2, 2] = del2_Nu(alpha, Nu, ff)
+    hess[1, 2] = cross_del_a_Nu(alpha, Nu, ff)
     hess[2, 1] = hess[1, 2]
     return hess
 end
 
 
-function hessian_log(log_alpha, log_Nu, n, ff)
+function hessian_log(log_alpha, log_Nu, ff)
     alpha = exp(log_alpha)
     Nu = exp(log_Nu)
-    H = hessian(alpha, Nu, n, ff)
-    score_a = gradient_log_a(log_alpha, log_Nu, n, ff)
-    score_Nu = gradient_log_Nu(log_alpha, log_Nu, n, ff)
+    H = hessian(alpha, Nu, ff)
+    score_a = gradient_log_a(log_alpha, log_Nu, ff)
+    score_Nu = gradient_log_Nu(log_alpha, log_Nu, ff)
     hess_log = zeros(2, 2)
     hess_log[1, 1] =  alpha^2 * H[1, 1] + score_a
     hess_log[2, 2] =  Nu^2 * H[2, 2] + score_Nu
@@ -181,15 +187,14 @@ end
 
 
 function fit_Gamma(theta0,
-    n,
     ff;
     lower::Vector = [-Inf, -Inf],
     upper::Vector = [10, 12],
     ftol = 1e-7
 )
-    L(x) = -loglh(x[1], x[2], n, ff)
-    g_Nu(x)  = -gradient_log_Nu(x[1], x[2], n, ff)
-    g_a(x) = -gradient_log_a(x[1], x[2], n, ff)
+    L(x) = -loglh(x[1], x[2], ff)
+    g_Nu(x)  = -gradient_log_Nu(x[1], x[2], ff)
+    g_a(x) = -gradient_log_a(x[1], x[2], ff)
     function objective(x::Vector, grad::Vector)
         if length(grad) > 0
             grad[1] = g_a(x)
